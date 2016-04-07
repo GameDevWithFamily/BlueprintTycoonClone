5030dd4c0168821e0a1f50a1bae1ffa0390d7f43
Samuel Walker <Samuel Walker>
2016-04-07 11:13:16 -0600

    worked with things!

---
+++ Assets/Intro/IntroScreen.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IntroScreen : MonoBehaviour {
+
+    MovieTexture[] intros;
+    MovieTexture currentIntro;
+    Material myMaterial;
+    int introIndex;
+
+	// Use this for initialization
+	void Start () {
+        intros = Resources.LoadAll<MovieTexture>("IntroVids/");
+        myMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentIntro == null || !currentIntro.isPlaying) {
+            if (introIndex < intros.Length)
+            {
+                currentIntro = intros[introIndex];
+                myMaterial.mainTexture = currentIntro;
+                myMaterial.SetTexture("_EmissionMap", currentIntro);
+                AudioSource src = GetComponent<AudioSource>();
+                while (!currentIntro.isReadyToPlay) {
+                }
+                AudioClip movieAudio = currentIntro.audioClip;
+                src.clip = movieAudio;
+
+                src.Stop();
+                src.Play();
+
+                currentIntro.Stop();
+                currentIntro.Play();
+                introIndex++;
+            }
+            else {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            }
+        }
+	}
+}
---
+++ Assets/IntroClip.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IntroClip : MonoBehaviour {
+
+
+    public MovieTexture[] textures;
+    int movieIndex = 0;
+    MovieTexture texture;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (texture == null || !texture.isPlaying) {
+            Debug.Log(textures.Length + " " + movieIndex);
+            if (movieIndex < textures.Length)
+            {
+                texture = textures[movieIndex];
+                this.GetComponent<Renderer>().material.mainTexture = texture;
+                texture.Stop();
+                texture.Play();
+                movieIndex++;
+            }
+            else {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+	}
+}
